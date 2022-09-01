0c5b7532e6d8011b4596ad8a828dff3570059296
hossein <derakhshanfar.hossein@gmail.com>
2022-09-01 12:08:26 +0300

    feat: setup apollo server

---
+++ src/application/index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer, Config } from 'apollo-server';
+import { makeExecutableSchema } from 'graphql-tools';
+import { handleGraphQLContext } from './auth/index';
+import { rawSchema } from './graphql';
+
+// create our schema
+const schema = makeExecutableSchema(rawSchema);
+
+// configure the server here
+const serverConfig: Config = {
+  schema,
+  context: handleGraphQLContext,
+  playground: {
+    settings: {
+      'editor.theme': 'dark', // change to light if you prefer
+      'editor.cursorShape': 'line', // possible values: 'line', 'block', 'underline'
+    },
+  },
+};
+
+// create a new server
+export default new ApolloServer(serverConfig);
